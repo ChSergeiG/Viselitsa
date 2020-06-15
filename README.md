b7ab595c78dd10de3f4d6467eab94d15240c0db7
Cherviakov Sergei <chsergeyg@gmail.com>
2020-06-15 14:19:52 +0300

    1.0.12. Coveralls

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Build Status](https://travis-ci.com/ChSergeiG/Viselitsa.svg?branch=master)](https://travis-ci.com/ChSergeiG/Viselitsa)
+[![Coverage Status](https://coveralls.io/repos/github/ChSergeiG/Viselitsa/badge.svg?branch=master)](https://coveralls.io/github/ChSergeiG/Viselitsa?branch=master)
 
 # Viselitsa
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,24 +6,34 @@ repositories {
 }
 
 plugins {
-    id("io.ratpack.ratpack-java") version "1.5.4"
+    jacoco
+    id("com.github.kt3k.coveralls") version "2.8.4"
     application
     kotlin("jvm") version "1.3.72"
 }
 
 group = "ru.chsergeig.bot.viselitsa"
-version = "1.0.11"
+version = "1.0.12"
+
+jacoco {
+    toolVersion = "0.8.4"
+}
+
+coveralls {
+    sourceDirs = listOf("src/main/kotlin")
+}
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
     implementation(group = "net.dv8tion", name = "JDA", version = "4.1.1_154")
     implementation(group = "com.jagrosh", name = "jda-utilities", version = "3.0.3")
     implementation(group = "com.mashape.unirest", name = "unirest-java", version = "1.4.9")
     implementation(group = "org.apache.httpcomponents", name = "httpclient", version = "4.1.1")
     implementation(group = "commons-lang", name = "commons-lang", version = "2.6")
     implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.11.0")
-    implementation(kotlin("stdlib-jdk8"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.6.2")
 }
 
 val compileKotlin: KotlinCompile by tasks
@@ -45,4 +55,16 @@ tasks.test {
     testLogging {
         events("passed", "skipped", "failed")
     }
+}
+
+tasks.jacocoTestReport {
+    dependsOn("test")
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = true
+    }
+}
+
+tasks.coveralls {
+    dependsOn("jacocoTestReport")
 }
